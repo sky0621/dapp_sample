f1b564a496001a3acd3694c709fdcd9b458d4b66
sky0621 <sky14work@gmail.com>
2023-11-04 21:10:20 +0900

    -

---
+++ scripts/deploy.ts
@@ -0,0 +1,15 @@
+import {ethers} from "hardhat";
+
+const ownerAddress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
+const main = async () => {
+    const MemberNFT = await ethers.getContractFactory("MemberNFT");
+    const memberNFT = await MemberNFT.deploy(ownerAddress);
+    await memberNFT.deployed();
+
+    console.info(`Contract deployed to: ${memberNFT.address}`)
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
