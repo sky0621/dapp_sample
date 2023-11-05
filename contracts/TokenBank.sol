f105e423c7364590f77d065dfd3f3872e0960eea
sky0621 <sky14work@gmail.com>
2023-11-05 12:02:12 +0900

    -

---
+++ contracts/TokenBank.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+contract TokenBank {
+    /// @dev Tokenの名前
+    string private _name;
+
+    /// @dev Tokenのシンボル
+    string private _symbol;
+
+    /// @dev Tokenの総供給数
+    uint256 constant _totalSupply = 1000;
+
+    /// @dev TokenBankが預かっているTokenの総額
+    uint256 private _bankTotalDeposit;
+
+    /// @dev TokenBankのオーナー
+    address public owner;
+
+    /// @dev アカウントアドレス毎のToken残高
+    mapping(address => uint256) private _balances;
+
+    /// @dev TokenBankが預かっているToken残高
+    mapping(address => uint256) private _tokenBankBalances;
+
+    /// @dev Token移転時のイベント
+    event TokenTransfer(
+        address indexed from,
+        address indexed to,
+        uint256 amount
+    );
+
+    /// @dev Token預入時のイベント
+    event TokenDeposit(
+        address indexed from,
+        uint256 amount
+    );
+
+    /// @dev Token引出時のイベント
+    event TokenWithdraw(
+        address indexed from,
+        uint256 amount
+    );
+}
